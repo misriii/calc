93eaa9a7b7366c3b6181c1f797ca631d05c4164b
misriii <misriyaamman10@gmail.com>
2023-07-18 17:59:47 +0530

    add js file

---
+++ calc.js
@@ -0,0 +1,15 @@
+function displayData(n){
+  result.value+=n
+}
+
+function check(){
+  result.value=eval(result.value)
+}
+
+function allClear(){
+  result.value=""
+}
+
+function minus(){
+  result.value=result.value.slice(0, -1)
+}
